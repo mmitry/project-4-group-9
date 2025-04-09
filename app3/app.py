37ef2c338af0d1b0f016c5ecfdd6026949f0204a
Adriana <kuhladriana@gmail.com>
2025-04-08 20:44:34 -0400

    frontend

--- app3/app.py
+++ app3/app.py
@@ -8,15 +8,36 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 modelHelper = ModelHelper()
 
 #Route to render index.html template using data from Monga
-@app.route("/")
-def home():
-    #Return template and data
-    return render_template("index.html")
-
 @app.route("/about_us")
 def about_us():
     return render_template("about_us.html")
 
+@app.route("/")
+def home():
+    #Return template and data
+    return render_template("home.html")
+
+@app.route("/predictor")
+def predictor():
+    return render_template("index.html")
+
+@app.route("/report")
+def report():
+    return render_template("report.html")
+
+@app.route("/tableau1")
+def tableau1():
+    return render_template("tableau1.html")
+
+@app.route("/tableau2")
+def tableau2():
+    return render_template("tableau2.html")
+
+@app.route("/works_cited")
+def works_cited():
+    return render_template("works_cited.html")
+
+#Predictor
 @app.route("/predictions", methods=["POST"])
 def predictions():
     content = request.json["data"]
